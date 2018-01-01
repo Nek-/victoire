6cc26f87fd83d32f13ab200921867d320b9a917d
Maxime Veber <nek.dev@gmail.com>
2018-01-01 18:53:00 +0100

    Updated dependencies

--- Bundle/I18nBundle/Translation/Translator.php
+++ Bundle/I18nBundle/Translation/Translator.php
@@ -3,16 +3,20 @@
 namespace Victoire\Bundle\I18nBundle\Translation;
 
 use Symfony\Bundle\FrameworkBundle\Translation\Translator as BaseTranslator;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Translation\MessageSelector;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class Translator extends BaseTranslator
 {
     protected $container;
     protected $options = [
-        'cache_dir'      => 'test',
-        'debug'          => true,
-        'resource_files' => [],
+        'cache_dir'             => 'test',
+        'debug'                 => true,
+        'resource_files'        => [],
+        'domains'               => [],
+        'default_kernel_locale' => 'en',
     ];
     protected $loaderIds;
 
@@ -22,15 +26,46 @@ class Translator extends BaseTranslator
     private $selector;
 
     /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
      * {@inheritdoc}
      *
      * @api
      */
-    public function __construct(ContainerInterface $container, MessageSelector $selector, $loaderIds = [], array $options = [])
+    public function __construct(RequestStack $requestStack, SessionInterface $session, ContainerInterface $container, MessageSelector $selector, $loaderIds = [], array $options = [])
     {
+        $this->computeOptions($options);
         parent::__construct($container, $selector, $loaderIds, $options);
         $this->selector = $selector;
         $this->container = $container;
+        $this->session = $session;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @var array $options Options given as parameter for Symfony translator to compute with local option.
+     * 
+     * @return void
+     */
+    private function computeOptions(array &$options)
+    {
+        if (isset($options['domains'])) {
+            $this->options['domains'] = $options['domains'];
+            unset($options['domains']);
+        }
+
+        if (isset($options['domains'])) {
+            $this->options['default_kernel_locale'] = $options['default_kernel_locale'];
+            unset($options['default_kernel_locale']);
+        }
     }
 
     /**
@@ -45,7 +80,7 @@ class Translator extends BaseTranslator
         }
         if (null === $domain) {
             $domain = 'messages';
-        } elseif (in_array($domain, $this->container->getParameter('victoire_i18n.users_locale.domains'))) {
+        } elseif (in_array($domain, $this->options['domains'])) {
             $locale = $this->getVictoireLocale();
         }
         if (!isset($this->catalogues[$locale])) {
@@ -67,7 +102,7 @@ class Translator extends BaseTranslator
         }
         if (null === $domain) {
             $domain = 'messages';
-        } elseif (in_array($domain, $this->container->getParameter('victoire_i18n.users_locale.domains'))) {
+        } elseif (in_array($domain, $this->options['domains'])) {
             $locale = $this->getVictoireLocale();
         }
         if (!isset($this->catalogues[$locale])) {
@@ -104,7 +139,7 @@ class Translator extends BaseTranslator
      */
     public function getVictoireLocale()
     {
-        $this->locale = $this->container->get('session')->get('victoire_locale');
+        $this->locale = $this->session->get('victoire_locale');
 
         return $this->locale;
     }
@@ -114,10 +149,10 @@ class Translator extends BaseTranslator
      */
     public function getCurrentLocale()
     {
-        if ($request = $this->container->get('request_stack')->getCurrentRequest()) {
+        if ($request = $this->requestStack->getCurrentRequest()) {
             return $request->getLocale();
         }
 
-        return $this->container->getParameter('kernel.default_locale');
+        return $this->options['default_kernel_locale'];
     }
 }
